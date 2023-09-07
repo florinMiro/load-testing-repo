be6d25bf0b3db14711c2b42b150648408402a05f
Florin Mirosnicencu <florin.mirosnicencu@snyk.io>
2023-09-07 10:41:48 +0100

    chore: add fake commit 0

--- commit.sh
+++ commit.sh
@@ -1,14 +1,14 @@
 baseHash="$(git rev-parse HEAD)"
 
-for i in {0..1}; do
+for i in {0..0}; do
   echo "new line $i" >> testFile.md
   git add *
   git commit -m "chore: add fake commit $i"
   gitHash="$(git rev-parse HEAD)"
   echo "{
-            "target": "florinMiro/testing-repo-95",
-            "oid": "$gitHash",
-            "baseOid": "384ae8e9d4b724d882cc26872970e952822d193d"
+            \"target\": \"florinMiro/testing-repo-95\",
+            \"oid\": \"$gitHash\",
+            \"baseOid\": \"384ae8e9d4b724d882cc26872970e952822d193d\"
           }," >> /Users/florinmirosnicencu/code/testing-repo-95/95p_repo_list.txt
   git push
 done
