b674a8703c7b9bd36cece42d7be62ba3bac3d04a
Florin Mirosnicencu <florin.mirosnicencu@snyk.io>
2023-09-07 09:39:14 +0100

    chore: add fake commit 1

---
+++ commit.sh
@@ -0,0 +1,10 @@
+baseHash="$(git rev-parse HEAD)"
+
+for i in {1..5}; do
+  echo "new line $i" >> testFile.md
+  git add *
+  git commit -m "chore: add fake commit $i"
+  gitHash="$(git rev-parse HEAD)"
+  echo "$gitHash," >> /Users/florinmirosnicencu/hashes.txt
+  git push
+done
